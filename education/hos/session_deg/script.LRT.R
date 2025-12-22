364826fda0a42dc1f71bb2b253a6f074942786e9
Adrián López García de Lomana <14022465+adelomana@users.noreply.github.com>
2025-12-22 14:20:15 +0000

    completed pipelines for LTR and Wald appropriately

--- education/hos/session_deg/script.LRT.R
+++ education/hos/session_deg/script.LRT.R
@@ -1,3 +1,5 @@
+rm(list = ls())
+
 #
 # -1. packages installation
 #
@@ -14,6 +16,7 @@
 # BiocManager::install('this.path')
 # BiocManager::install('ramify')
 # BiocManager::install('crayon')
+# BiocManager::install('apeglm')
 
 library(tximport)       # required to read input files
 library(DESeq2)         # the library that will call DEGs
@@ -39,8 +42,8 @@ results_dir = 'DEGs_DESeq2'
 # 
 # 1. get todays working data: kallisto output from two conditions
 #
-system('wget https://ireigogn.hi.is/index.php/s/Lj9APBfmgXiTpWY/download/kallisto_output.tgz')
-untar('kallisto_output.tgz')
+#system('wget https://ireigogn.hi.is/index.php/s/Lj9APBfmgXiTpWY/download/kallisto_output.tgz')
+#untar('kallisto_output.tgz')
 
 list.files('kallisto_output')
 list.files('kallisto_output/WT_with_IFN_1/')
@@ -50,8 +53,9 @@ View(df)
 #
 # 2. get annotation mapping from transcript to genes
 #
-system('wget https://github.com/pachterlab/kallisto-transcriptome-indices/releases/download/v1/human_index_standard.tar.xz')
-untar('human_index_standard.tar.xz')
+#system('wget https://github.com/pachterlab/kallisto-transcriptome-indices/releases/download/v1/human_index_standard.tar.xz')
+#untar('human_index_standard.tar.xz')
+
 t2g = read.csv('t2g.txt', sep='\t', header = FALSE)
 View(t2g)
 
@@ -75,13 +79,11 @@ View(metadata)
 # 4. bring the expression profiles into DESeq2
 #
 txi = tximport(metadata$path, type="kallisto", tx2gene=t2g)
+
 dds = DESeqDataSetFromTximport(txi, colData=metadata, design=~treatment) 
 dds$treatment = relevel(dds$treatment, ref="without")
-
-#
-# 5. minimal filter on undetected genes: at least 10 reads in three samples.
+# minimal filter on undetected genes: at least 10 reads in three samples.
 # additional filters should happen a posteriori, because removing a lot of genes would affect DESeq2 assumptions
-#
 cat(blue(paste('size before counts filtering:', dim(dds)[1], sep=' ')), fill=TRUE)
 smallestGroupSize <- 3
 keep <- rowSums(counts(dds) >= 10) >= smallestGroupSize
@@ -89,51 +91,82 @@ dds <- dds[keep,]
 cat(blue(paste('size after counts filtering:', dim(dds)[1], sep=' ')), fill=TRUE)
 
 #
-# 6. Statisical test
+# 6. Statistical test
 #
-# LRT and Wald tests are available, more info here: https://hbctraining.github.io/DGE_workshop_salmon/lessons/08_DGE_LRT.html
-dds = DESeq(dds, test="LRT", reduced=~1)
 
-# retrieve significant results
-res = results(dds, parallel=TRUE, alpha=0.05) 
-res1 = res[which(res$padj < 0.05), ]
+# Fit model (Wald test by default)
+dds <- DESeq(dds, test = "LRT", reduced = ~ 1)
 
-#
-# 7. filter DEGs that are very lowly expressed, below an average normalized counts of 50, baseMean >= 50
-#
-cat(blue(paste('size before filtering:', dim(res1)[1], sep=' ')), fill=TRUE)
-norm_counts <- counts(dds, normalized = TRUE)[rownames(res1), ]
-keep <- rowMeans(norm_counts) >= 50
-res2 = res1[keep, ]
-cat(blue(paste('size after filtering:', dim(res2)[1], sep=' ')), fill=TRUE)
+# 6.1.get p-values / padj for H0: log2FC = 0
+resultsNames(dds)
+res <- results(dds, alpha = 0.05)
 
-#
-# 8. filter DEGs that do not show great difference at the count level, even more strict than previous one
-#
-# filter results that do not have at least 50 counts difference
-cat(blue(paste('size before filtering:', dim(res2)[1], sep=' ')), fill=TRUE)
-a = counts(dds, normalize=TRUE)[rownames(res2), 1:3]
-b = counts(dds, normalize=TRUE)[rownames(res2), 4:6]
-c = rowMedians(a) - rowMedians(b)
-keep = abs(c) >= 50
-res3 = res2[keep, ]
-cat(blue(paste('size after filtering:', dim(res3)[1], sep=' ')), fill=TRUE)
+# 6.2 Shrink LFC for stable effect-size reporting
+res_shr <- lfcShrink(dds, coef = "treatment_with_vs_without", type = "apeglm")
+# visualize shrinkage
+plotMA(res, ylim = c(-5, 5))
+plotMA(res_shr, ylim = c(-5, 5))
 
-#
-# 9. filter on log2FC of one for final results
-#
-cat(blue(paste('size before filtering:', dim(res3)[1], sep=' ')), fill=TRUE)
-filtred_results = res3[abs(res3$log2FoldChange) >= 1, ]
-sorted_filtred_results = filtred_results[order(filtred_results[["padj"]]),]
-cat(blue(paste('size after filtering:', dim(sorted_filtred_results)[1], sep=' ')), fill=TRUE)
+# 6.3 Build a final table with:
+#    - padj from res0 (correct for the H0: LFC = 0 test)
+#    - shrunken LFC from res0_shr (recommended for reporting/thresholding)
+full_results <- data.frame(
+  gene_id = rownames(res),
+  baseMean = res$baseMean,
+  log2FC_MLE = res$log2FoldChange,
+  lfcSE = res$lfcSE,
+  stat = res$stat,
+  pvalue = res$pvalue,
+  padj = res$padj,
+  log2FC_shr = res_shr$log2FoldChange,
+  stringsAsFactors = FALSE
+)
 
-# get anti results for plotting
-anti_results = res[which(res$padj > 0.05 | abs(res$log2FoldChange) < 1), ]
+# add counts differences 
+condition_col <- "treatment"     # change if your metadata column differs
+level_A <- "with"                # change to your level name
+level_B <- "without"             # change to your level name
+stopifnot(condition_col %in% colnames(colData(dds)))
+stopifnot(all(c(level_A, level_B) %in% as.character(unique(colData(dds)[[condition_col]]))))
+norm_counts <- counts(dds, normalized = TRUE)
+idx_A <- which(colData(dds)[[condition_col]] == level_A)
+idx_B <- which(colData(dds)[[condition_col]] == level_B)
+median_A <- rowMedians(norm_counts[, idx_A, drop = FALSE])
+median_B <- rowMedians(norm_counts[, idx_B, drop = FALSE])
+delta_counts <- median_A - median_B
+names(delta_counts) <- rownames(norm_counts)
+full_results$delta_counts <- delta_counts[match(full_results$gene_id, names(delta_counts))]
+
+tpm_mat <- txi$abundance
+median_TPM_A <- matrixStats::rowMedians(tpm_mat[, idx_A, drop = FALSE])
+median_TPM_B <- matrixStats::rowMedians(tpm_mat[, idx_B, drop = FALSE])
+names(median_TPM_A) <- rownames(tpm_mat)
+names(median_TPM_B) <- rownames(tpm_mat)
+full_results$median_TPM_A <- median_TPM_A[full_results$gene_id]
+full_results$median_TPM_B <- median_TPM_B[full_results$gene_id]
+
+# 6.4 Example: final “biologically relevant” subset (you define thresholds)
+responders <- subset(
+  full_results,
+  !is.na(padj) &
+    padj < 0.05 &
+    abs(log2FC_shr) >= 1 &
+    abs(delta_counts) >= 50
+)
+responders <- responders[order(responders$padj), ]
+
+# no response
+no_responders <- full_results[!full_results$gene_id %in% responders$gene_id, ]
+
+# report values
+dim(full_results)
+dim(no_responders)
+sum(full_results$padj < 0.05, na.rm = TRUE)
+dim(responders)
 
 # store results
 dir.create(results_dir)
-write.table(sorted_filtred_results, file=paste(results_dir, '/effect_IFN_vs_noIFN.tsv', sep=''), quote=FALSE, sep='\t')
-write.table(anti_results, file=paste(results_dir, '/effect_IFN_vs_noIFN.anti.tsv', sep=''), quote=FALSE, sep='\t')
+write.table(full_results, file=paste(results_dir, '/effect_IFN_vs_noIFN.tsv', sep=''), quote=FALSE, sep='\t')
 
 #
 # 5. visualization
@@ -142,17 +175,17 @@ write.table(anti_results, file=paste(results_dir, '/effect_IFN_vs_noIFN.anti.tsv
 # 5.1. a simple PCA
 plotPCA(rlog(dds), intgroup=c('treatment')) + ggtitle('effect IFN vs control')
 
-# 5.2.a volcano plot
-plotting_x = sorted_filtred_results$log2FoldChange
-y = sorted_filtred_results$padj
+# 5.2. a simple volcano plot
+plotting_x = responders$log2FC_shr
+y = responders$padj
 epsilon = min(y[y !=0])
 plotting_y = -log10(y + epsilon) ## why???
 df = data.frame(plotting_x=plotting_x, plotting_y=plotting_y)
 reds = df[df$plotting_x > 0, ]
 blues = df[df$plotting_x < 0, ]
 
-plotting_x = anti_results$log2FoldChange
-plotting_y = -log10(anti_results$padj)
+plotting_x = no_responders$log2FC_shr
+plotting_y = -log10(no_responders$padj)
 blacks = data.frame(plotting_x=plotting_x, plotting_y=plotting_y)
 
 ggplot() + 
@@ -162,44 +195,14 @@ ggplot() +
   labs(x='log2FC', y='-log10 adj P') + 
   theme_linedraw() 
        
-#               
-# 5.3. a rather centered volcano
 #
-plotting_x = sorted_filtred_results$log2FoldChange
-y = sorted_filtred_results$padj
+# 5.4. a rather elaborated volcano including TPM values
+#
+plotting_x = responders$log2FC_shr
+y = responders$padj
 epsilon = min(y[y !=0])
 plotting_y = -log10(y + epsilon) 
-df = data.frame(plotting_x=clip(plotting_x, .min=-8, .max=8), plotting_y=clip(plotting_y, .min=0, .max=50))
-reds = df[df$plotting_x > 0, ]
-blues = df[df$plotting_x < 0, ]
-
-plotting_x = anti_results$log2FoldChange
-plotting_y = -log10(anti_results$padj)
-blacks = data.frame(plotting_x=clip(plotting_x, .min=-8, .max=8), plotting_y=clip(plotting_y, .min=0, .max=50))
-
-ggplot() + 
-  geom_point(data=reds, aes(plotting_x, plotting_y), color = "red", size=3, shape=19, alpha=0.5, stroke=0) + 
-  geom_point(data=blues, aes(plotting_x, plotting_y), color = "blue", size=3, shape=19, alpha=0.5, stroke=0) +
-  geom_point(data=blacks, aes(plotting_x, plotting_y), color = "black", size=1, shape=19, alpha=0.2, stroke=0) +
-  labs(x=expression('Expression [log'[2]~'FC]'), y=expression('Significance [log'[10]~'adjusted P]')) + 
-  theme_linedraw() +
-  geom_segment(aes(x=-1, xend=-1, y=-log10(0.05), yend=50), linetype=2) +
-  geom_segment(aes(x=1, xend=1, y=-log10(0.05), yend=50), linetype=2) +
-  geom_segment(aes(x=-8, xend=-1, y=-log10(0.05), yend=-log10(0.05)), linetype=2) +
-  geom_segment(aes(x=1, xend=8, y=-log10(0.05), yend=-log10(0.05)), linetype=2) +
-  xlim(-8.2, 8) 
-
-
-#
-# 5.4. a volcano including TPM values
-#
-plotting_x = sorted_filtred_results$log2FoldChange
-
-y = sorted_filtred_results$padj
-epsilon = min(y[y !=0])
-plotting_y = -log10(y + epsilon) 
-
-plotting_z = log10(rowMeans(txi$abundance[rownames(sorted_filtred_results), ]))
+plotting_z = log10(rowMeans(responders[, c('median_TPM_A', 'median_TPM_B')]))
 
 df = data.frame(plotting_x=clip(plotting_x, .min=-8, .max=8), 
                 plotting_y=clip(plotting_y, .min=0, .max=50),
@@ -208,14 +211,14 @@ df = data.frame(plotting_x=clip(plotting_x, .min=-8, .max=8),
 reds = df[df$plotting_x > 0, ]
 blues = df[df$plotting_x < 0, ]
 
-plotting_x = anti_results$log2FoldChange
-plotting_y = -log10(anti_results$padj)
+plotting_x = no_responders$log2FC_shr
+plotting_y = -log10(no_responders$padj)
 blacks = data.frame(plotting_x=clip(plotting_x, .min=-8, .max=8), plotting_y=clip(plotting_y, .min=0, .max=50))
 
 ggplot() + 
   geom_point(data=reds, aes(x=plotting_x, y=plotting_y, color=plotting_z), , size=3, shape=19, alpha=2/3, stroke=0) + 
   geom_point(data=blues, aes(plotting_x, plotting_y, color=plotting_z), size=3, shape=19, alpha=2/3, stroke=0) +
-  geom_point(data=blacks, aes(plotting_x, plotting_y), color = "black", size=1, shape=19, alpha=0.2, stroke=0) +
+  geom_point(data=blacks, aes(plotting_x, plotting_y), color = "black", size=1, shape=19, alpha=1/3, stroke=0) +
   labs(x=expression('Expression difference [log'[2]~'FC]'), y=expression('Significance [log'[10]~'adjusted P]'), color=expression('Expression average [log'[10]~'TPM]')) + 
   theme_linedraw() +
   geom_segment(aes(x=-1, xend=-1, y=-log10(0.05), yend=50), linetype=2) +
