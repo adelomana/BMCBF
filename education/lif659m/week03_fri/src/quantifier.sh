0681ec11c110961ebde7aadf3eb52b2b1d010d9e
Adrián López García de Lomana <14022465+adelomana@users.noreply.github.com>
2026-02-06 00:44:02 +0000

    exercise completed

---
+++ education/lif659m/week03_fri/src/quantifier.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -euo pipefail # a command to fail if any step in a pipe fails
+
+#
+# 0. preliminaries
+#
+
+# install kallisto
+cd software
+
+wget https://github.com/pachterlab/kallisto/releases/download/v0.51.1/kallisto_linux-v0.51.1.tar.gz
+tar xvf kallisto_linux-v0.51.1.tar.gz
+
+# retrive the reference transcriptome index for zebra fish
+wget https://github.com/pachterlab/kallisto-transcriptome-indices/releases/download/v1/zebrafish_index_standard.tar.xz
+tar xvf zebrafish_index_standard.tar.xz
+
+# inspect the data using head. They are FASTQ files, paired FASTQ files, so two paired files describe one sample. Note the paired naming on the reads
+
+
+#
+# 1. quantify a transcriptome using only one core.
+#
+
+#
+# 2. quantify a transcriptome using two threads
+#
---
+++ education/lif659m/week03_fri/src/solutions.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -euo pipefail # a command to fail if any step in a pipe fails
+
+#
+# 0. preliminaries
+#
+
+# install kallisto
+cd software
+
+wget https://github.com/pachterlab/kallisto/releases/download/v0.51.1/kallisto_linux-v0.51.1.tar.gz
+tar xvf kallisto_linux-v0.51.1.tar.gz
+
+# retrive the reference transcriptome index for zebra fish
+wget https://github.com/pachterlab/kallisto-transcriptome-indices/releases/download/v1/zebrafish_index_standard.tar.xz
+tar xvf zebrafish_index_standard.tar.xz
+
+# inspect the data. They are FASTQ files, paired FASTQ files, so two paired files describe one sample. Note the paired naming on the reads
+cd ../data
+head *
+cd ..
+
+#
+# 1. quantify a transcriptome using only one core.
+#
+time software/kallisto/kallisto quant -i software/index.idx -o output data/sample_1_pair_1.fq data/sample_1_pair_2.fq -t 1 -b 100
+
+#
+# 2. quantify a transcriptome using two threads
+#
+time software/kallisto/kallisto quant -i software/index.idx -o output data/sample_1_pair_1.fq data/sample_1_pair_2.fq -t 2 -b 100
